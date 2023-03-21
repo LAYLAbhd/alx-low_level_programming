43824032641fd13feafb64080023ce9f8a2c440c
LAYLAbhd <bouhad.layla@outlook.fr>
2023-03-21 11:45:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ *main - void
  *
- * Return: Always 0 (Success)
- */
+ *Return: 0
+ *
+ **/
+
 int main(void)
 {
+	int a = 0;
 	char str[] = "_putchar";
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[a] >= a)
 	{
-		_putchar(str[i]);
+	_putchar(str[a]);
+	a++;
 	}
 	_putchar('\n');
 	return (0);
