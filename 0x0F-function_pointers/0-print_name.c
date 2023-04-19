8f4a822eaff85528a027e970118091d20c55b3ba
LAYLAbhd <bouhad.layla@outlook.fr>
2023-04-19 15:25:06 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,13 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - a name to print 
+ * print_name - prints a name by using poniter function 
  * @name: name to print
  * @f: pointer to the printing function
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name && !f)
+	if (!name || !f)
 		return;
 	f(name);
 }
