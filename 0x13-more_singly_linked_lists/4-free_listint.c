26cb1543bc307d434a4f718b583202ddbf497beb
LAYLAbhd <bouhad.layla@outlook.fr>
2023-04-29 03:09:26 -0700

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,40 +1,17 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - add a new node at the end
- * of a linked list
+ * free_listint - frees a linked list
  * @head: head of a list.
- * @n: n element.
  *
- * Return: address of the new element. NUll if it failed.
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+ * Return: no return.
+ */void free_listint(listint_t *head)
 {
-	listint_t *new;
 	listint_t *temp;
 
-	(void)temp;
-
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-	temp = *head;
-	if (*head == NULL)
+	while ((temp = head) != NULL)
 	{
-		*head = new;
+		head = head->next;
+		free(temp);
 	}
-	else
-	{
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		temp->next = new;
-	}
-
-	return (*head);
 }
